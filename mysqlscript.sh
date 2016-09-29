5512c92340554e9220146186484907875b06df90
chingchong <norbuurgen@gmail.com>
2016-09-29 21:50:38 +0545

    Update mysqlscript.sh

--- mysqlscript.sh
+++ mysqlscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-MYPWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo $MYPWD > /root/.mysqlrootpasswd
-mysql -e "create database easy_forms; create database sitebuilder;"
-mysql easy_forms < /var/www/easyforms/local/easy_forms.sql
-mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYPWD}');"
+MYPWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1| awk '{ print $1; }')
+echo $MYPWD >> /root/.mysqlrootpasswd
+mysql -uroot -ppass123 -e "create database easy_forms; create database sitebuilder;"
+mysql -uroot -ppass123  easy_forms < /var/www/easyforms/local/easy_forms.sql
+mysql -uroot -ppass123 -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYPWD}');"
+
